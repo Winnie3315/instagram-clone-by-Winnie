15a646dbe65a1bedda74370c92219f75ecff72a1
Winnie3315 <winniiemae777@gmail.com>
2024-06-19 17:56:51 +0500

    darkmode

--- js/main.js
+++ js/main.js
@@ -83,30 +83,37 @@ function reload(posts, users, place) {
         postLike.classList.add('post-like');
         const likeBtn = document.createElement('button');
         likeBtn.classList.add('like-btn');
-        const likeImg = document.createElement('img');
-        likeImg.src = './img/notification_black.svg';
-        likeImg.alt = 'like';
-        likeBtn.append(likeImg);
+        likeBtn.innerHTML = `<svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M17.3 2.05002C20.15 2.05002 22.5 4.65002 22.5 7.80002C22.5 11.2 19.55 13.3 16.75 15.8C13.95 18.3 12.5 19.65 12 19.95C11.45 19.6 9.65 17.95 7.25 15.8C4.4 13.3 1.5 11.2 1.5 7.80002C1.5 4.65002 3.85 2.05002 6.7 2.05002C8.8 2.05002 9.95 3.05002 10.75 4.20002C11.7 5.50002 11.85 6.15002 12 6.15002C12.15 6.15002 12.3 5.50002 13.25 4.20002C14.05 3.05002 15.2 2.05002 17.3 2.05002ZM17.3 0.550018C15.05 0.550018 13.35 1.45002 12 3.35002C10.65 1.50002 8.95 0.600018 6.7 0.600018C3 0.550018 0 3.80002 0 7.80002C0 11.45 2.7 13.8 5.3 16.05C5.6 16.3 5.95 16.6 6.25 16.9L7.4 17.9C9.6 19.85 10.7 20.85 11.2 21.15C11.45 21.3 11.75 21.4 12 21.4C12.3 21.4 12.55 21.3 12.8 21.15C13.3 20.85 14.2 20.05 16.7 17.75L17.7 16.85C18.05 16.55 18.35 16.25 18.7 16C21.35 13.8 24 11.5 24 7.80002C24 3.80002 21 0.550018 17.3 0.550018Z" fill="var(--color-switcher)"/>
+</svg>
+                            `
         postLike.append(likeBtn);
 
         const postComment = document.createElement('div');
         postComment.classList.add('post-comment');
         const commentBtn = document.createElement('button');
         commentBtn.classList.add('comment-btn');
-        const commentImg = document.createElement('img');
-        commentImg.src = './img/comment.svg';
-        commentImg.alt = 'comment';
-        commentBtn.append(commentImg);
+        commentBtn.innerHTML = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g clip-path="url(#clip0_2_1652)">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M23.75 23.05L22.35 17.55C23.25 15.9 23.75 14 23.75 12C23.75 5.5 18.5 0.25 12 0.25C5.5 0.25 0.25 5.5 0.25 12C0.25 18.5 5.5 23.75 12 23.75C14 23.75 15.9 23.25 17.55 22.35L23.05 23.75C23.45 23.85 23.85 23.45 23.75 23.05ZM22.25 12C22.25 14 21.75 15.5 20.95 17C20.85 17.2 20.8 17.45 20.85 17.7L21.9 21.9L17.75 20.85C17.5 20.8 17.25 20.8 17.05 20.95C16.15 21.45 14.45 22.25 12.05 22.25C6.35 22.25 1.75 17.65 1.75 12C1.75 6.35 6.35 1.75 12 1.75C17.65 1.75 22.25 6.35 22.25 12Z" fill="white"/>
+                                </g>
+                                <defs>
+                                <clipPath id="clip0_2_1652">
+                                <rect width="24" height="24" fill="white"/>
+                                </clipPath>
+                                </defs>
+                                </svg>
+                            `
+
         postComment.append(commentBtn);
 
         const postShare = document.createElement('div');
         postShare.classList.add('post-share');
         const shareBtn = document.createElement('button');
         shareBtn.classList.add('share-btn');
-        const shareImg = document.createElement('img');
-        shareImg.src = './img/share.svg';
-        shareImg.alt = 'share';
-        shareBtn.append(shareImg);
+        shareBtn.innerHTML = `<svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M23.9 0.9C23.75 0.65 23.5 0.5 23.25 0.5H0.75C0.45 0.55 0.15 0.75 0.05 1C-0.05 1.25 0 1.6 0.2 1.85L8.15 9.65L10.9 20.95C10.95 21.25 11.2 21.45 11.5 21.5H11.6C11.85 21.5 12.1 21.35 12.25 21.15L23.85 1.65C24.05 1.45 24.05 1.15 23.9 0.9ZM2.6 2.05H20.35L9 8.35L2.6 2.05ZM11.95 18.85L9.75 9.65L21.2 3.3L11.95 18.85Z" fill="white"/>
+</svg>`
         postShare.append(shareBtn);
         postInfoBox.append(postLike, postComment, postShare);
 
@@ -114,10 +121,9 @@ function reload(posts, users, place) {
         postFavorite.classList.add('post-favorite');
         const favoriteBtn = document.createElement('button');
         favoriteBtn.classList.add('favorite-btn');
-        const favoriteImg = document.createElement('img');
-        favoriteImg.src = './img/favorite.svg';
-        favoriteImg.alt = 'favorite';
-        favoriteBtn.append(favoriteImg);
+        favoriteBtn.innerHTML = `<svg width="22" height="24" viewBox="0 0 22 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M20.75 24C20.55 24 20.35 23.9 20.2 23.8L11 14.5L1.8 23.8C1.6 24 1.25 24.1 1 23.95C0.7 23.85 0.5 23.55 0.5 23.25V0.75C0.5 0.35 0.85 0 1.25 0H20.75C21.15 0 21.5 0.35 21.5 0.75V23.25C21.5 23.55 21.3 23.85 21.05 23.95C20.95 24 20.85 24 20.75 24ZM11 13C11.4 13 11.8 13.15 12.1 13.45L20 21.45V1.5H2V21.45L9.9 13.45C10.2 13.15 10.6 13 11 13Z" fill="white"/>
+</svg>`
         postFavorite.append(favoriteBtn);
         postInfo.append(postInfoBox, postFavorite);
 
